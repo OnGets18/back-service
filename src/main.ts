c7f6ecf9a31130bb6672c6fab0f13eeb78102cd7
ongets <on@compie.co.il>
2025-12-14 22:57:26 +0200

    Trigger rebuild with version 1.6

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('Back Service API')
     .setDescription('API documentation for Back Service')
-    .setVersion('1.6')
+    .setVersion('1.6') // Current API version
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
